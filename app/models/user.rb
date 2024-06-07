f2958ca6dad727b40ab78378fd520430eaef013c
kayke <kayke.simao@gmail.com>
2024-06-06 21:22:33 -0300

    Validação para :name adicionado

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :incomes
+  validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
