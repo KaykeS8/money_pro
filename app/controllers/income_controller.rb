d8da952023e580f055c7a60844e1c11f49a86ca5
kayke <kayke.simao@gmail.com>
2024-06-10 21:10:03 -0300

    Ação create criada

--- app/controllers/income_controller.rb
+++ app/controllers/income_controller.rb
@@ -1,6 +1,23 @@
 class IncomeController < ApplicationController
     def index
+        @incomes = Income.all
     end
 
-    def new;end
+    def new
+        @income = Income.new
+    end
+
+    def create
+        @income = current_user.incomes.create(income_params)
+        if @income.save!
+            redirect_to entradas_path, status: :created
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def income_params
+        params.require(:income).permit(:title, :value, :description)
+    end
 end
