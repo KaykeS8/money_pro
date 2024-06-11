b72aa1b4e10c2cbdeb52db49188ab2e3e4acd9d4
kayke <kayke.simao@gmail.com>
2024-06-10 21:11:59 -0300

    Rota para criaçã de um income adicionado

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get '/entradas', to: 'income#index'
   get '/entradas/new', to: 'income#new'
+  post "/entradas", to: 'income#create'
   root to: 'home#index'
 end
