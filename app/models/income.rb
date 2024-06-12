54f12f8bee9fa7ac6b952fb20ed16b213edac850
kayke <kayke.simao@gmail.com>
2024-06-11 21:39:01 -0300

    Método para formatar moeda criado

--- app/models/income.rb
+++ app/models/income.rb
@@ -2,4 +2,8 @@ class Income < ApplicationRecord
     validates :title, presence: true
     validates :value, presence: true
     belongs_to :user
+
+    def price
+        ActionController::Base.helpers.number_to_currency(self[:value], :unit => "R$ ", :separator => ",", :delimiter => ".")
+    end
 end
